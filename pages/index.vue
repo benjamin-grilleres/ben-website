35b95e8311343db8f64e756e38c12fe0b5be602d
Benjamin Grilleres <bg@wecom4u.com>
2020-03-08 09:20:21 +0100

    init empty repository

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-
+  <section class="flex">
+    <div class="col-6 p-0 bckg-img" v-bckg-img="'/img/ben.JPG'" style="background-size: cover;background-position: center;background-repeat: no-repeat; height: 100vh" >
+    </div>
+    <div class="col-6 flex items-center justify-center">
+      <h1 style="color: #1D1F19; font-weight:bold;font-size: 50px">Hello, this is Benjamin</h1>
     </div>
   </section>
 </template>
