80431660b0c2328c599eeac2b712f41695231eca
Benjamin Grilleres <bg@wecom4u.com>
2020-04-09 09:50:48 +0200

    change order projects portfolio page

--- helpers/projects.js
+++ helpers/projects.js
@@ -9,6 +9,22 @@ export default [
     content: 'Bubbleflat est une application web qui permet aux étudiants et jeunes actifs de se regrouper par affinités ou modes de vie pour trouver un logement.'
   },
   {
+    company: 'Wecom4u',
+    img: '/img/portfolio/background/wefiles4u.png',
+    title: 'Wefiles4u',
+    color: '#4C4B4B',
+    content: 'Application interne permettant de partager rapidement des articles et des ressources (polices, mockups etc...).',
+    technos: ['Laravel', 'Vue.js', 'Nuxt.js', 'Mysql', 'Spatie']
+  },
+  {
+    company: 'Wecom4u',
+    img: '/img/portfolio/background/batiminute.png',
+    color: '#F5B31B',
+    title: 'Batiminute',
+    content: 'Application web et mobile permettant de réserver du matériel de BTP.',
+    technos: ['Laravel', 'React Native', 'Bluetooth LE', 'Android Studio', 'Xcode', 'Expo', 'Mysql']
+  },
+  {
     company: 'Bubbleflat',
     img: '/img/portfolio/background/stud.png',
     external_url: 'http://studnhouse.com',
@@ -18,22 +34,12 @@ export default [
     content: 'Après le pivot en Janvier 2018, Bubbleflat a changé de business et s\'est adressé aux écoles afin de proposer la plateforme en marque blanche.'
   },
   {
-    company: 'Tmic',
-    img: '/img/portfolio/background/siloe.png',
-    color: '#7F91C1',
-    external_url: 'https://librairiedupincerais.fr',
-    title: 'Librairie Siloe Lis Nantes',
-    content: 'La librairie Siloe Lis est une librairie généraliste et spécialisée dans la littérature religieuse.',
-    technos: ['PHP5', 'Thelia 1', 'Serveurs dédiés', 'SCSS', 'Mysql']
-  },
-  {
-    company: 'Tmic',
-    img: '/img/portfolio/background/pincerais.png',
-    color: '#A85B71',
-    external_url: 'https://librairiedupincerais.fr',
-    title: 'Librairie du Pincerais',
-    content: 'La librairie du Pincerais avait besoin à court terme d\'être présent sur Internet afin de vendre des livres ailleurs qu\'en magasin.',
-    technos: ['PHP5', 'Thelia 1', 'Serveurs dédiés', 'SCSS', 'Mysql']
+    company: 'Wecom4u',
+    color: '#C3BDBD',
+    img: '/img/portfolio/background/ejt.png',
+    title: 'EJT Toulouse',
+    content: 'Site web interne à l\'école de journalisme de Toulouse permettant aux étudiants et professeurs de créer et partager des articles, postuler à des offres d\'emploi et accéder à un annuaire de l\'école.',
+    technos: ['Laravel', 'Vue.js', 'Nuxt.js', 'Mysql', 'Spatie']
   },
   {
     company: 'Wecom4u',
@@ -55,14 +61,6 @@ export default [
   },
   {
     company: 'Wecom4u',
-    img: '/img/portfolio/background/wefiles4u.png',
-    title: 'Wefiles4u',
-    color: '#4C4B4B',
-    content: 'Application interne permettant de partager rapidement des articles et des ressources (polices, mockups etc...).',
-    technos: ['Laravel', 'Vue.js', 'Nuxt.js', 'Mysql', 'Spatie']
-  },
-  {
-    company: 'Wecom4u',
     img: '/img/portfolio/rossini.png',
     external_url: 'https://rossini-estimations.com/',
     title: 'Rossini estimations',
@@ -71,21 +69,22 @@ export default [
     technos: ['Vue.js','Nuxt.js','Librairie d\'animation', 'Hotjar', 'Invision']
   },
   {
-    company: 'Wecom4u',
-    color: '#C3BDBD',
-    img: '/img/portfolio/background/ejt.png',
-    title: 'EJT Toulouse',
-    content: 'Site web interne à l\'école de journalisme de Toulouse permettant aux étudiants et professeurs de créer et partager des articles, postuler à des offres d\'emploi et accéder à un annuaire de l\'école.',
-    technos: ['Laravel', 'Vue.js', 'Nuxt.js', 'Mysql', 'Spatie']
+    company: 'Tmic',
+    img: '/img/portfolio/background/siloe.png',
+    color: '#7F91C1',
+    external_url: 'https://www.librairie-siloe-lis-nantes.fr/',
+    title: 'Librairie Siloe Lis Nantes',
+    content: 'La librairie Siloe Lis est une librairie généraliste et spécialisée dans la littérature religieuse.',
+    technos: ['PHP5', 'Thelia 1', 'Serveurs dédiés', 'SCSS', 'Mysql']
   },
   {
-    company: 'Wecom4u',
-    img: '/img/portfolio/background/batiminute.png',
-    color: '#F5B31B',
-    title: 'Batiminute',
-    content: 'Application web et mobile permettant de réserver du matériel de BTP.',
-    technos: ['Laravel', 'React Native', 'Bluetooth LE', 'Android Studio', 'Xcode', 'Expo', 'Mysql']
+    company: 'Tmic',
+    img: '/img/portfolio/background/pincerais.png',
+    color: '#A85B71',
+    external_url: 'https://librairiedupincerais.fr',
+    title: 'Librairie du Pincerais',
+    content: 'La librairie du Pincerais avait besoin à court terme d\'être présent sur Internet afin de vendre des livres ailleurs qu\'en magasin.',
+    technos: ['PHP5', 'Thelia 1', 'Serveurs dédiés', 'SCSS', 'Mysql']
   },
-
 
 ]
